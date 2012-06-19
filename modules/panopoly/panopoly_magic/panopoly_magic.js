d000e44e88a51e4386a96b643927b680cd3f52c1
Matt Cheney <matt@getpantheon.com>
2012-06-19 09:58:32 -0700

    #1646012 from juanramonperez to improve h1.title replacement js logic

--- modules/panopoly/panopoly_magic/panopoly_magic.js
+++ modules/panopoly/panopoly_magic/panopoly_magic.js
@@ -9,7 +9,7 @@
       * Replaces the markup of a node title pane with
       * the h1.title page element
       */
-     if ($('.pane-node-title .pane-content').val() == $('h1.title').val()) {
+     if ($.trim($('.pane-node-title .pane-content').html()) == $.trim($('h1.title').html())) {
        $('.pane-node-title .pane-content').html('').prepend($('h1.title'));
      }
 
