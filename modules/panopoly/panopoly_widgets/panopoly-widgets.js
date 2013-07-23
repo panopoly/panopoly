baa4ebcf5315e8e52f08141aeafd119bf51e87b5
Matt Cheney <matt@getpantheon.com>
2013-07-23 16:54:59 -0700

    #2047953 - Updating Media and File Entity to Lates Devs: Part Four

--- modules/panopoly/panopoly_widgets/panopoly-widgets.js
+++ modules/panopoly/panopoly_widgets/panopoly-widgets.js
@@ -11,24 +11,26 @@ Drupal.settings.spotlight_settings = Drupal.settings.spotlight_settings || {};
   * @return
   *   The total displacement of given region in pixels.
   */
-  Drupal.overlay.getDisplacement = function (region) {
-    var displacement = 0;
-    var lastDisplaced = $('.overlay-displace-' + region + ':last');
-    if (lastDisplaced.length) {
-      displacement = lastDisplaced.offset().top + lastDisplaced.outerHeight();
+  if (Drupal.overlay) {
+    Drupal.overlay.getDisplacement = function (region) {
+      var displacement = 0;
+      var lastDisplaced = $('.overlay-displace-' + region + ':last');
+      if (lastDisplaced.length) {
+        displacement = lastDisplaced.offset().top + lastDisplaced.outerHeight();
 
-      // In modern browsers (including IE9), when box-shadow is defined, use the
-      // normal height.
-      var cssBoxShadowValue = lastDisplaced.css('box-shadow');
-      var boxShadow = (typeof cssBoxShadowValue !== 'undefined' && cssBoxShadowValue !== 'none');
-      // In IE8 and below, we use the shadow filter to apply box-shadow styles to
-      // the toolbar. It adds some extra height that we need to remove.
-      if (!boxShadow && /DXImageTransform\.Microsoft\.Shadow/.test(lastDisplaced.css('filter'))) {
-        displacement -= lastDisplaced[0].filters.item('DXImageTransform.Microsoft.Shadow').strength;
-        displacement = Math.max(0, displacement);
+        // In modern browsers (including IE9), when box-shadow is defined, use the
+        // normal height.
+        var cssBoxShadowValue = lastDisplaced.css('box-shadow');
+        var boxShadow = (typeof cssBoxShadowValue !== 'undefined' && cssBoxShadowValue !== 'none');
+        // In IE8 and below, we use the shadow filter to apply box-shadow styles to
+        // the toolbar. It adds some extra height that we need to remove.
+        if (!boxShadow && /DXImageTransform\.Microsoft\.Shadow/.test(lastDisplaced.css('filter'))) {
+          displacement -= lastDisplaced[0].filters.item('DXImageTransform.Microsoft.Shadow').strength;
+          displacement = Math.max(0, displacement);
+        }
       }
-    }
-    return displacement;
+      return displacement;
+    };
   };
 
  /**
