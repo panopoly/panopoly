3bd69db8991ea98b6403c3c0a16868dea418a811
David Snopek <dsnopek@gmail.com>
2013-12-03 19:16:52 +0000

    Issue #2148879: Write Behat tests for Panopoly spotlight widget

--- tests/behat/features/bootstrap/FeatureContext.php
+++ tests/behat/features/bootstrap/FeatureContext.php
@@ -3,6 +3,7 @@
 use Behat\Behat\Context\ClosuredContextInterface,
     Behat\Behat\Context\TranslatedContextInterface,
     Behat\Behat\Context\BehatContext,
+    Behat\Behat\Event\StepEvent,
     Behat\Behat\Exception\PendingException;
 use Behat\Gherkin\Node\PyStringNode,
     Behat\Gherkin\Node\TableNode;
@@ -20,16 +21,16 @@ use Drupal\DrupalExtension\Context\DrupalContext;
  */
 class FeatureContext extends DrupalContext
 {
-    /**
-     * Initializes context.
-     * Every scenario gets it's own context object.
-     *
-     * @param array $parameters context parameters (set them up through behat.yml)
-     */
-    public function __construct(array $parameters)
-    {
-        // Initialize your context here
-    }
+  /**
+   * Initializes context.
+   * Every scenario gets it's own context object.
+   *
+   * @param array $parameters context parameters (set them up through behat.yml)
+   */
+  public function __construct(array $parameters) {
+    // Initialize your context here
+    $this->useContext('panels', new PanelsSubContext());
+  }
 
 //
 // Place your definition and hook methods here:
@@ -42,4 +43,43 @@ class FeatureContext extends DrupalContext
 //        doSomethingWith($argument);
 //    }
 //
+
+  /**
+   * @AfterStep @javascript
+   *
+   * After every step in a @javascript scenario, we want to wait for AJAX
+   * loading to finish.
+   */
+  public function afterStep(StepEvent $event) {
+    if ($event->getResult() === 0) {
+      $this->iWaitForAJAX();
+    }
+  }
+
+  /**
+   * @Given /^Panopoly magic live previews are disabled$/
+   *
+   * Disable live previews via Panopoly Magic.
+   */
+  public function disablePanopolyMagicLivePreview() {
+    $this->getDriver('drush')->vset('panopoly_magic_live_preview 0 --yes');
+  }
+
+  /**
+   * @Given /^(?:|I )wait(?:| for) (\d+) seconds?$/
+   *
+   * Wait for the given number of seconds. ONLY USE FOR DEBUGGING!
+   */
+  public function iWaitForSeconds($arg1) {
+    sleep($arg1);
+  }
+
+  /**
+   * @Given /^(?:|I )wait for AJAX loading to finish$/
+   *
+   * Wait for the jQuery AJAX loading to finish. ONLY USE FOR DEBUGGING!
+   */
+  public function iWaitForAJAX() {
+    $this->getSession()->wait(5000, 'jQuery.active === 0');
+  }
 }
