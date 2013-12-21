1c6eedcfe632407e8dd07b67b62b58f9dad14b4c
Tom Kirkpatrick <tom@systemseed.com>
2013-12-21 19:49:07 +0100

    Add a third option to rebuild in release mode in rebuild.sh

--- rebuild.sh
+++ rebuild.sh
@@ -18,19 +18,25 @@ if [ -f drupal-org.make ]; then
   echo '================='
 
   echo "\nThis command can be used to rebuild the installation profile in place.\n"
-  echo "  [1] Rebuild profile in place in release mode"
-  echo "  [2] Rebuild profile in place in development mode (with .git working-copy)\n"
+  echo "  [1] Rebuild profile in place in release mode (latest stable release)"
+  echo "  [2] Rebuild profile in place in development mode (latest dev code)"
+  echo "  [3] Rebuild profile in place in development mode (latest dev code with .git working-copy)\n"
   echo "Selection: \c"
   read SELECTION
 
   if [ $SELECTION = "1" ]; then
 
-    echo "Building Panopoly install profile..."
-    drush make --no-core --no-gitinfofile --contrib-destination=. drupal-org.make
+    echo "Building Panopoly install profile in release mode..."
+    drush make --no-core --no-gitinfofile --contrib-destination=. drupal-org-release.make
 
   elif [ $SELECTION = "2" ]; then
 
-    echo "Building Panopoly install profile..."
+    echo "Building Panopoly install in development mode (latest dev code)..."
+    drush make --no-core --no-gitinfofile --contrib-destination=. drupal-org.make
+
+  elif [ $SELECTION = "3" ]; then
+
+    echo "Building Panopoly install profilein development mode (latest dev code with .git working-copy)"
     drush make --working-copy --no-core --no-gitinfofile --contrib-destination=. drupal-org-dev.make
 
   else
