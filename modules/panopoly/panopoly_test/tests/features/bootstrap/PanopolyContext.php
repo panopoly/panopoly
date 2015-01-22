82da8b1b0aa53cdf8181b7a50006288bce45546c
cboyden <cboyden@657902.no-reply.drupal.org>
2015-01-22 23:29:40 +0000

    Issue #2268647 by cboyden, dsnopek: File upload tests do not clean up after themselves

--- modules/panopoly/panopoly_test/tests/features/bootstrap/PanopolyContext.php
+++ modules/panopoly/panopoly_test/tests/features/bootstrap/PanopolyContext.php
@@ -24,6 +24,13 @@ class PanopolyContext extends DrupalContext
 {
 
   /**
+   * Keep track of files added by tests so they can be cleaned up.
+   *
+   * @var array
+   */
+  public $files = array();
+
+  /**
    * Initializes context.
    * Every scenario gets its own context object.
    *
@@ -87,4 +94,112 @@ class PanopolyContext extends DrupalContext
     }
     return $argument;
   }
+
+  /**
+   * @Given /^the managed file "([^"]*)"$/
+   *
+   * This function copies the provided file into the site files directory,
+   * creates a file object with the URI, and passes that object to a file
+   * creation function to create the entity.
+   * The function has to be here for now, as it needs some Mink functions.
+   *
+   * @todo: See if it can be done without Mink functions?
+   * @todo: Allow creating private files
+   * @todo: Add before and after event dispatchers
+   * @todo: Add ability to create multiple files at once using Table
+   */
+  public function createFile($filename, $public = TRUE) {
+    // Get location of source file.
+    if ($this->getMinkParameter('files_path')) {
+      $source_path = rtrim(realpath($this->getMinkParameter('files_path'))) . DIRECTORY_SEPARATOR . $filename;
+      if (!is_file($source_path)) {
+        throw new \Exception(sprintf("Cannot find the file at '%s'", $source_path));
+      }
+    } else {
+      throw new \Exception("files_path not set");
+    }
+
+    $prefix = $public ? 'public://' : 'private://';
+    $uri = $prefix . $filename;
+
+    $this->fileCreate($source_path, $uri);
+  }
+
+  /**
+   * Create a managed Drupal file.
+   *
+   * @param $source_path
+   *   A file object passed in with the URI already set
+   * @param $destination
+   *   (Optional) The desired URI where the file will be uploaded.
+   *
+   * @return
+   *   Drupal file object.
+   */
+  public function fileCreate($source_path, $destination = NULL) {
+    $data = file_get_contents($source_path);
+
+    // Before working with files, we need to change our current directory to
+    // DRUPAL_ROOT so that the relative paths that define the stream wrappers
+    // (like public:// or temporary://) actually work.
+    $cwd = getcwd();
+    chdir(DRUPAL_ROOT);
+
+    if ($file = file_save_data($data, $destination)) {
+      $this->files[] = $file;
+    }
+
+    // Then change back.
+    chdir($cwd);
+
+    return $file;
+  }
+
+  /**
+   * Cleans up files after every scenario.
+   *
+   * @AfterScenario
+   */
+  public function cleanUpFiles($event) {
+    // Add any files created by test users to the $files variable.
+    if (!empty($this->users)) {
+      foreach ($this->users as $user) {
+        $uids[] = $user->uid;
+      }
+      $file_ids = db_query('SELECT fid FROM {file_managed} WHERE uid IN (:uids)', array(':uids' => $uids))->fetchAll();
+      if (!empty($file_ids)) {
+        // file_delete() expects an object.
+        foreach ($file_ids as $fid) {
+          $file = file_load($fid->fid);
+          $this->files[] = $file;
+        }
+      }
+    }
+
+    // Delete any files that were created by test users or our Given step.
+    if (!empty($this->files)) {
+      foreach ($this->files as $file) {
+        $this->fileDelete($file);
+      }
+    }
+
+    $this->files = array();
+  }
+
+  /**
+   * Delete a managed Drupal file.
+   *
+   * @param $file
+   *   A file object to delete
+   */
+  public function fileDelete($file) {
+    // See PanopolyContext::fileCreate() for information on why we do this.
+    $cwd = getcwd();
+    chdir(DRUPAL_ROOT);
+
+    file_delete($file, TRUE);
+
+    chdir($cwd);
+  }
+
 }
