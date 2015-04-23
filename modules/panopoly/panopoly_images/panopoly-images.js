191e02e6d4fd50acbdcb5080ff5b73472e37b38c
dsnopek <dsnopek@266527.no-reply.drupal.org>
2015-04-23 15:02:29 +0100

    Issue #2476227 by dsnopek: Page full of variables after changing layout

--- modules/panopoly/panopoly_images/panopoly-images.js
+++ modules/panopoly/panopoly_images/panopoly-images.js
@@ -1,30 +1,28 @@
 (function($) {
   Drupal.behaviors.panopolyImagesModule = {
     attach: function (context, settings) {
-      var captions = $('.caption', context);
-      $(captions).once('panopoly-images').imagesLoaded( function () {
+      var captions = $('.caption', context).has('img');
+      $(captions).once('panopoly-images').imagesLoaded(function () {
         panopolyImagesResizeCaptionBox(captions);
       });
 
       function panopolyImagesResizeCaptionBox(captions) {
-        captions.each(function( index ) {
-          var imageSet = $('img', this);
-          var imgBoxWidth = 0;
-          var imgWidth = 0;
-          imgBoxWidth = getImgWidth(imageSet);
-          var wrapperBoxWidth =
-              getWrapperSpacing($('.caption-inner'))
-            + getWrapperSpacing($('.caption-width-container'));
-          var totalWidth = imgBoxWidth + wrapperBoxWidth;
+        captions.each(function() {
+          var imageSet = $('img', this),
+              imgBoxWidth = getImgWidth(imageSet),
+              wrapperBoxWidth =
+                  getWrapperSpacing($('.caption-inner', this))
+                + getWrapperSpacing($('.caption-width-container', this)),
+              totalWidth = imgBoxWidth + wrapperBoxWidth;
           $(this).width(totalWidth);
         });
       }
 
       // Get width of image plus margins, borders and padding
       function getImgWidth(imageSet) {
-        var imgWidth = 0;
-        var imgBoxExtra = 0;
-        var testWidth = 0;
+        var imgWidth = 0,
+            imgBoxExtra = 0,
+            testWidth = 0;
 
         // We shouldn't have more than one image in a caption, but it would be
         // possible, so we make sure we have the widest one
@@ -41,18 +39,17 @@
 
       // We want the total of margin, border and padding on the element
       function getWrapperSpacing(el) {
-        var spacing = ['margin-left', 'border-left', 'padding-left', 'padding-right', 'border-right', 'margin-right'];
-        var totalPx = 0;
-        var spacePx = 0;
-        var spaceRaw = '';
-        var i = 0;
-        for (i = 0; i < 6; i++) {
+        var spacing = ['margin-left', 'border-left', 'padding-left', 'padding-right', 'border-right', 'margin-right'],
+            totalPx = 0,
+            spacePx = 0,
+            spaceRaw = '';
+        for (var i = 0; i < spacing.length; i++) {
           spaceRaw = $(el).css(spacing[i]);
 
           // Themers might add padding, borders or margin defined in ems, but we can't
           // add that to pixel dimensions returned by naturalWidth, so we just throw
           // away anything but pixels. Themers have to deal with that.
-          if(spaceRaw.substr(spaceRaw.length - 2) == 'px') {
+          if(spaceRaw && spaceRaw.substr(spaceRaw.length - 2) == 'px') {
             spacePx = parseInt(spaceRaw, 10);
             totalPx += ($.isNumeric(spacePx)) ? spacePx : 0;
           }
