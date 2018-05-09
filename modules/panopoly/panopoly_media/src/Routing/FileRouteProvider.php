4cea62295186d855c59344b0d98159858925eaae
Will Long <will@kerasai.com>
2018-05-09 10:49:57 -0500

    added action and confirm form for multiple file deletion

--- modules/panopoly/panopoly_media/src/Routing/FileRouteProvider.php
+++ modules/panopoly/panopoly_media/src/Routing/FileRouteProvider.php
@@ -18,6 +18,7 @@ class FileRouteProvider implements EntityRouteProviderInterface {
   public function getRoutes(EntityTypeInterface $entity_type) {
     $route_collection = new RouteCollection();
 
+    // Individual file delete form.
     $route = (new Route('/file/{file}/delete'))
       ->addDefaults([
         '_entity_form' => 'file.delete',
@@ -27,6 +28,14 @@ class FileRouteProvider implements EntityRouteProviderInterface {
       ->setRequirement('_entity_access', 'file.delete');
 
     $route_collection->add('entity.file.delete_form', $route);
+
+    // Bulk operation confirmation form.
+    $route = (new Route('/admin/content/files/delete'))
+      ->addDefaults([
+        '_form' => '\Drupal\panopoly_media\Form\FileDeleteMultiple',
+      ])
+      ->setRequirement('_permission', 'access files overview');
+    $route_collection->add('file.multiple_delete_confirm', $route);
 
     return $route_collection;
   }
