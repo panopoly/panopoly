cc3bdc61b944121d208c818a96c86653a098d03b
David Snopek <dsnopek@gmail.com>
2019-05-22 13:55:23 -0500

    Try installing Behat in the top-level composer.json

--- .drupal-ti/runners/panopoly-behat/install.sh
+++ .drupal-ti/runners/panopoly-behat/install.sh
@@ -20,8 +20,7 @@ panopoly_build_distribution
 
 # Install the behat extension.
 panopoly_header Installing Behat
-cd "$DRUPAL_TI_DRUPAL_DIR/$DRUPAL_TI_BEHAT_DIR"
-composer install --no-interaction --prefer-source --dev
+composer require drupal/drupal-extension:^3.2.2 --no-interaction
 
 # Disable sendmail
 # @todo Move upstream.
